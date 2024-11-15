7d1ffd88d12c5ebb27f6754514bb72062f07c928
amrelkhodary <amrelkhodarybusiness@gmail.com>
2024-11-15 08:39:32 +0200

    added tic-tac-toe game logic

--- src/script.js
+++ src/script.js
@@ -23,11 +23,51 @@ function Game(firstPlayer, secondPlayer, board, turn, currentStatus) {
     turn = 1;
 
     const updateStatus = function() {
-        /**
-         * implement logic to inspect the board to see if a player has won
+        /*
+         * I'm doing it using a brute force method, because honsetly this problem is too simple
+         * to think of a clever algorithm for it and my midterms start tomorrow :)
          */
 
-        
+        //check for player_1 win
+        if(board.board[0][0] == 1 && board.board[0][1] == 1 && board.board[0][2] == 1 ||
+           board.board[1][0] == 1 && board.board[1][1] == 1 && board.board[1][2] == 1 ||
+           board.board[2][0] == 1 && board.board[2][1] == 1 && board.board[2][2] == 1 ||
+           board.board[0][0] == 1 && board.board[1][0] == 1 && board.board[2][0] == 1 ||
+           board.board[0][1] == 1 && board.board[1][1] == 1 && board.board[2][1] == 1 ||
+           board.board[0][2] == 1 && board.board[1][2] == 1 && board.board[2][2] == 1 ||
+           board.board[0][0] == 1 && board.board[1][1] == 1 && board.board[2][2] == 1 ||
+           board.board[0][2] == 1 && board.board[1][1] == 1 && board.board[2][0] == 1 
+        ) {
+            currentStatus = STATUS_PLAYER_ONE_WINS;
+            return;
+        }
+        //check for player_2 win
+        else if(board.board[0][0] == 2 && board.board[0][1] == 2 && board.board[0][2] == 2 ||
+            board.board[1][0] == 2 && board.board[1][1] == 2 && board.board[1][2] == 2 ||
+            board.board[2][0] == 2 && board.board[2][1] == 2 && board.board[2][2] == 2 ||
+            board.board[0][0] == 2 && board.board[1][0] == 2 && board.board[2][0] == 2 ||
+            board.board[0][1] == 2 && board.board[1][1] == 2 && board.board[2][1] == 2 ||
+            board.board[0][2] == 2 && board.board[1][2] == 2 && board.board[2][2] == 2 ||
+            board.board[0][0] == 2 && board.board[1][1] == 2 && board.board[2][2] == 2 ||
+            board.board[0][2] == 2 && board.board[1][1] == 2 && board.board[2][0] == 2 
+         ) {
+             currentStatus = STATUS_PLAYER_TWO_WINS;
+             return;
+         }
+
+        //check for tie
+        else if(board.board[0][0] != -1 && board.board[0][1] != -1 && board.board[0][2] != -1 ||
+            board.board[1][0] != -1 && board.board[1][1] != -1 && board.board[1][2] != -1 ||
+            board.board[2][0] != -1 && board.board[2][1] != -1 && board.board[2][2] != -1 ||
+            board.board[0][0] != -1 && board.board[1][0] != -1 && board.board[2][0] != -1 ||
+            board.board[0][1] != -1 && board.board[1][1] != -1 && board.board[2][1] != -1 ||
+            board.board[0][2] != -1 && board.board[1][2] != -1 && board.board[2][2] != -1 ||
+            board.board[0][0] != -1 && board.board[1][1] != -1 && board.board[2][2] != -1 ||
+            board.board[0][2] != -1 && board.board[1][1] != -1 && board.board[2][0] != -1 
+         ) {
+             currentStatus = STATUS_TIE;
+             return;
+         } 
     }
     
     const updateTurn = function() {
